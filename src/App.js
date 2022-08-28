decccd81d0a1217d70894c471377bee99418a388
nathanguevara <nathan.guevara@gmail.com>
2022-08-28 11:07:03 -0500

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,94 @@
-import './App.css';
-import Button from '@mui/material/Button'
+import { useState, useEffect } from "react";
+import Header from "./components/header";
+import Input from "./components/input";
+import Button from "@mui/material/Button";
+import Cards from "./components/cards";
+import Switch from "./components/switch";
+import Slider from "./components/slider";
+import Select from "./components/select";
+
+import "./App.css";
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  const [isLogged, setIsLogged] = useState(false);
+  const [isToggled, setIsToggled] = useState(false);
+  const [value, setValue] = useState(30);
+  const [quality, setQuality] = useState(2);
+
+  useEffect(() => {
+    console.log(isToggled, value);
+  }, [isToggled, value]);
+
+  return (
+    <main>
+      <Header title="My Music App" />
+      <div className="app">
+        {!isLogged ? (
+          <form className="form">
+            <Input label="Username" />
+            <Input label="password" type="password" />
+            <Button
+              className="button"
+              sx={{ mt: "8px" }}
+              variant="contained"
+              onClick={() => setIsLogged(true)}
+            >
+              Login
+            </Button>
+          </form>
+        ) : (
+          <div className="dashboard-container">
+            <h1 style={{ color: "grey" }}>Welcome User!</h1>
+            <div className="card-container">
+              <Cards
+                state={isToggled}
+                setState={setIsToggled}
+                title="Online Mode"
+                body="Is this application connected to the internet?"
+                component={Switch}
+              />
+              <Cards
+                className
+                state={value}
+                setState={setValue}
+                title="Master Volume"
+                body="Overrides all other sound setting in this application"
+                component={Slider}
+              />
+              <Cards
+                state={quality}
+                setState={setQuality}
+                title="Master Volume"
+                body="Overrides all other sound setting in this application"
+                component={Select}
+              />
+            </div>
+            <div className="system-notifications-container">
+              <h3>System Notifications:</h3>
+              {isToggled && (
+                <p>
+                  Your application is offline. You won't be able to share or
+                  stream music to other devices.
+                </p>
+              )}
+              {value >= 80 && (
+                <p>
+                  Listening to music at a high volume could cause long-term
+                  hearing loss.
+                </p>
+              )}
+              {quality < 2 && (
+                <p>
+                  Music quality is degraded. Increase quality if your connection
+                  allows it.
+                </p>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/input.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function Input(props) {
+  return (
+    <Box component="form" noValidate autoComplete="off">
+      <TextField
+        required
+        InputLabelProps={{ required: false }}
+        id={props.id}
+        label={props.label}
+        type={props.type ? props.type : "text"}
+        defaultValue={props.defaultValue}
+        variant="standard"
+      />
+    </Box>
+  );
+}
